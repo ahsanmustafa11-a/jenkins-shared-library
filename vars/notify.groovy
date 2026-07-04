df1049278a4e9af5392f1c25a81c62b5ee079dc4
Ahsan Mustafa <139356111+ahsanmustafa11-a@users.noreply.github.com>
2026-07-05 02:43:21 +0500

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,77 +1,63 @@
+def call(String email, String status) {
+
+    if (status == 'SUCCESS') {
+        success(email)
+    } else {
+        failed(email)
+    }
+
+}
+
 def success(String email) {
 
     emailext(
-
         to: email,
-
         subject: "SUCCESS : ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-
         mimeType: 'text/html',
-
         attachmentsPattern: '''
 reports/*.json,
 reports/*.html,
 dependency-check-report.*
 ''',
-
         body: """
-
 <h2 style="color:green;">Build Successful</h2>
 
 <b>Job:</b> ${env.JOB_NAME}<br>
-
 <b>Build:</b> #${env.BUILD_NUMBER}<br>
-
-<b>URL:</b>
+<b>Result:</b> ${currentBuild.currentResult}<br>
+<b>URL:</b><br>
 
 <a href="${env.BUILD_URL}">
-
 ${env.BUILD_URL}
-
 </a>
-
 """
-
     )
 
 }
 
-def failed(String email){
+def failed(String email) {
 
     emailext(
-
         to: email,
-
         subject: "FAILED : ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-
         mimeType: 'text/html',
-
         attachmentsPattern: '''
 reports/*.json,
 reports/*.html,
 dependency-check-report.*
 ''',
-
         body: """
-
 <h2 style="color:red;">Build Failed</h2>
 
 <b>Job:</b> ${env.JOB_NAME}<br>
-
 <b>Build:</b> #${env.BUILD_NUMBER}<br>
-
-<b>URL:</b>
+<b>Result:</b> ${currentBuild.currentResult}<br>
+<b>URL:</b><br>
 
 <a href="${env.BUILD_URL}">
-
 ${env.BUILD_URL}
-
 </a>
-
 """
-
     )
 
 }
-
-return this
