464fb7771e3346f88ef8b39e199252dfd239cd4c
Ahsan Mustafa <139356111+ahsanmustafa11-a@users.noreply.github.com>
2026-07-05 00:45:59 +0500

    docker-login-push.groovy

---
+++ vars/docker-login-push.groovy
@@ -0,0 +1,18 @@
+def call(String credentialsId) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: "${credentialsId}",
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+
+        sh '''
+            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+             docker compose push
+        '''
+
+    }
+
+}
